af739769ca0aac9eecb56c3da0e4313d2550d75f
abdulmaleekadams <harbdulmaleek@gmail.com>
2022-12-14 04:31:17 -0800

    Add banner component, banner css module

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const CoffeeStore = () => {
+  return (
+    <div>
+      Coffee Store Page
+      <Link href="/">Back to home</Link>
+    </div>
+  );
+};
+
+export default CoffeeStore;
